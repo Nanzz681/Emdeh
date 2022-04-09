51224c3a0cd4830f09d08abadac941d2966b2fbd
Nanzz681 <adnanputra681@gmail.com>
2022-04-09 16:46:51 +0700

    Emdeh

---
+++ settings.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	alfa: 'https://zenzapi.xyz',
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://zenzapi.xyz': '08142281da',
+}
+
+// setting 
+global.autoread = false // auto read pesan / message
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = true //status online (online)
+
+// Other
+global.botname = "XynanzzBotz" //namabot kalian
+global.ownername= "NanzzWangsaff" //nama kalian
+global.myweb ="https://github.com/Nanzz681" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.github = "https://github.com/Nanzz681" //bebas
+global.email = "adnanputra@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.ownernomer = "6282140059454" // nomor wa kalian
+global.ownernomerr = "+6282140059454" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6282140059454"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Nanzz' //sticker wm ubah
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n' //sticker wm ganti nama kalian
+global.sessionName = 'session'
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: 'Done Banh ',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: 'Proses.....',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
